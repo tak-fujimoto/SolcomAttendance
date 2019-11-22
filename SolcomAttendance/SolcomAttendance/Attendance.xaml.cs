0869c2e2fccaebb027fd98e56f97e1120ae3e2ac
tak-fujimoto <43066892+tak-fujimoto@users.noreply.github.com>
2019-11-22 13:46:12 +0900

    Revert "ADD"
    This reverts commit 47fbc1518ebce6a695db9b6b91fa9bd4e006ef21.

--- SolcomAttendance/SolcomAttendance/Attendance.xaml.cs
+++ SolcomAttendance/SolcomAttendance/Attendance.xaml.cs
@@ -6,31 +6,17 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using XamForms.Controls;
 
 namespace SolcomAttendance
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
-        Monthly NowMonth;
-        DisplayDay NowDay;
-
         public Page1(string name)
         {
             InitializeComponent();
 
-            NowMonth = new Monthly(DateTime.Now);
-            NowDay = new DisplayDay(NowMonth.GetDay(DateTime.Now));
-
             label.Text = name;
-            SelectedDay.BindingContext = NowDay;
-        }
-
-        private void OnDateClickHandler(object sender, DateTimeEventArgs e)
-        {
-            var ClickedDay = NowMonth.GetDay(e.DateTime);
-            NowDay.ChangeDay(ClickedDay);
         }
     }
 }
